5bd9bf839daa364f466a9f08eb5de601dee177c7
Asdaois <josexx1234@gmail.com>
2021-11-28 12:34:56 -0400

    feat: Define athore controllers

---
+++ controllers/authorController.js
@@ -0,0 +1,39 @@
+// Display list of all Authors.
+exports.list = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author list');
+};
+
+// Display detail page for a specific Author.
+exports.detail = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+};
+
+// Display Author create form on GET.
+exports.create_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author create GET');
+};
+
+// Handle Author create on POST.
+exports.create_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author create POST');
+};
+
+// Display Author delete form on GET.
+exports.delete_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author delete GET');
+};
+
+// Handle Author delete on POST.
+exports.delete_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author delete POST');
+};
+
+// Display Author update form on GET.
+exports.update_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author update GET');
+};
+
+// Handle Author update on POST.
+exports.update_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author update POST');
+};

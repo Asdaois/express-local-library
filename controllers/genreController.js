a959fb98a732607ae115d279eeeccc90b1252425
Asdaois <josexx1234@gmail.com>
2021-11-29 10:55:05 -0400

    feat: Implement create genre controller and view

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,5 +1,5 @@
 const Genre = require("../models/genre");
-const Book = require("../models/book")
+const Book = require("../models/book");
 const async = require("async");
 // Display list of all Genre.
 exports.list = (req, res) => {
@@ -44,13 +44,43 @@ exports.detail = (req, res, next) => {
 
 // Display Genre create form on GET.
 exports.create_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre create GET");
+  res.render("genre/form", { title: "Create Genre" });
 };
 
+const { body, validationResult } = require("express-validator");
 // Handle Genre create on POST.
-exports.create_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre create POST");
-};
+exports.create_post = [
+  body("name", "Genre name required").trim().isLength({ min: 1 }).escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const genre = new Genre({ name: req.body.name });
+
+    if (!errors.isEmpty()) {
+      // There are errors
+      // Render the form again with sanitized values / error messages.
+      res.render("genre/form", {
+        title: "Create Genre",
+        errors: errors.array(),
+        genre,
+      });
+      return;
+    }
+
+    Genre.findOne({ name: req.body.name }).exec((err, found_genre) => {
+      if (err) return next(err);
+
+      if (found_genre) {
+        res.redirect(found_genre.url);
+      } else {
+        genre.save((err) => {
+          if (err) return next(err);
+          res.redirect(genre.url);
+        });
+      }
+    });
+  },
+];
 
 // Display Genre delete form on GET.
 exports.delete_get = (req, res) => {
